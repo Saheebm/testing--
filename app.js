17f8a0d342b73ab2e91d0f79193e3ae0f56bd9d3
saheebm <saheebmuheez00@gmail.com>
2024-07-07 10:40:06 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+
+
+// var value = 'some value';
+// value = 'some other value';
+// let name = 'john';
+// name = 'peter';
+// const lastName = 'smith';
+
+// console.log(value);
+// console.log(name);
+// console.log(lastName);
+
+
+
+// let name = "john shripm taco v";
+// let address, xip, state;
+// address = '101 main street';
+// zip = '60612';
+// state = 'ca';
+// name = 'fish burrito';
+// console.log(address, zip, state);
+// console.log(name);
+
+
+// document.write({name: 'john'});
+// alert({name: 'john'});
+// console.log({name: 'john'});
